96b56c4df06a2d10cac7c36c52193fb0721c2498
magdamiu <badita.magda@gmail.com>
2020-11-24 21:22:09 +0200

    Course 2: Views | second part

--- app/src/main/java/com/android/androidfundamentalsgroup1/MainActivity.java
+++ app/src/main/java/com/android/androidfundamentalsgroup1/MainActivity.java
@@ -3,27 +3,87 @@ package com.android.androidfundamentalsgroup1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView oneTextView;
+    private EditText editTextUserFullName;
+    private CheckBox checkBoxTermsAndConditions;
+    private SeekBar seekBarCountChallenges;
+    private TextView textViewPurpleContent;
+    private Button buttonGetContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // CTRL/CMD + B = go to definition
-        // CTRL + Q = show documentation
-        // CTRL + P = show the params of a method
-        // CTRL + ALT + L = format the code
-        // TODO finish the implementation
+        // seteaza layout-ul asociat activatii MainActivity
+        // setContentView(R.layout.activity_main);
+        // asociem views_sample_1 ca layout al activitatii MainActivity
+        setContentView(R.layout.views_sample_1);
 
-        int countStudents = 28;
-        if (countStudents % 2 == 0) {
-            countStudents++;
-            Log.e("MainActivity", "numar par"); // afisat cu rosu, log pentru eroare
-            Log.d("MainActivity", countStudents + ""); // afisat cu gri, log pentru debug
-        } else if (countStudents % 3 == 0) {
-            Log.i("MainActivity", "numar divizibil cu 3");
+        oneTextView = findViewById(R.id.firstTextView);
+        oneTextView.setText(R.string.new_text);
+
+        editTextUserFullName = findViewById(R.id.editTextFullName);
+        editTextUserFullName.setText(R.string.default_full_name);
+
+        checkBoxTermsAndConditions = findViewById(R.id.checkboxTermsAndConditions);
+        if (checkBoxTermsAndConditions.isChecked()) {
+            checkBoxTermsAndConditions.setChecked(false);
+            oneTextView.setText(R.string.checkbox_checked);
+        } else {
+            checkBoxTermsAndConditions.setChecked(true);
+            oneTextView.setText(R.string.checkbox_unchecked);
+        }
+
+        checkBoxTermsAndConditions.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (compoundButton.isChecked()) {
+                    editTextUserFullName.setText(R.string.checkbox_state_checked);
+                } else {
+                    editTextUserFullName.setText(R.string.checkbox_state_unchecked);
+                }
+            }
+        });
+
+        seekBarCountChallenges = findViewById(R.id.seekBarChallenges);
+        seekBarCountChallenges.setProgress(5);
+
+        textViewPurpleContent = findViewById(R.id.textViewPurpleContent);
+
+        buttonGetContent = findViewById(R.id.buttonGetContent);
+        // block comment/un-comment CTRL + Shift + /
+        /*buttonGetContent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // preluam contentul din EditText si il convertim in String
+                String contentFromEditText = editTextUserFullName.getText().toString();
+                if (contentFromEditText != null && contentFromEditText.length() > 0) {
+                    textViewPurpleContent.setText(contentFromEditText);
+                } else {
+                    editTextUserFullName.setError(getString(R.string.error_missing_text));
+                }
+            }
+        });*/
+
+    }
+
+
+    public void buttonGetContentOnClick(View view) {
+        // preluam contentul din EditText si il convertim in String
+        String contentFromEditText = editTextUserFullName.getText().toString();
+        if (contentFromEditText != null && contentFromEditText.length() > 0) {
+            textViewPurpleContent.setText(contentFromEditText);
+        } else {
+            editTextUserFullName.setError(getString(R.string.error_missing_text));
         }
     }
 }
